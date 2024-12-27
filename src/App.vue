326457ce22786b7c6d43c7346865cf45906b4574
xjt <290548940@qq.com>
2024-12-27 09:24:53 +0800

    'feat:示例更新'

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <div class="canvasBox">
+      <Grid :mockData="mockData"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { mockData } from './components/mock';
+import Grid from './components/Grid.vue';
+</script>
+
+<style scoped>
+.canvasBox{
+  width: 100%;
+  height: 100%;
+}
+.wrapper{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #000;
+}
+</style>
